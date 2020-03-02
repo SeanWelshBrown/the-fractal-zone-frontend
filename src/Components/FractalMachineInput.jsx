803925c2c014d024c32fa3cf39905d0a68e1b459
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-03-02 17:19:28 -0500

    Got one form of Fractal input working
    X and Y coordinates

--- src/Components/FractalMachineInput.jsx
+++ src/Components/FractalMachineInput.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
 
 const FractalMachineInput = (props) => {
+  
 
     return (
         <div>
           <h3>Input!</h3>
           <p>This is where the user will give input to make a fractal.</p>
+
+          {/* <label>X: </label>
+          <input 
+            type="number" 
+            name="X" 
+            placeholder="Enter a number..." 
+            value={props.userInputX} 
+            onChange={props.handleUserInput} 
+          />
+          <br/>
+          <label>Y: </label>
+          <input 
+            type="number" 
+            name="Y" 
+            placeholder="Enter a number..." 
+            value={props.userInputY} 
+            onChange={props.handleUserInput} 
+          /> */}
+          
+          <input 
+            type="range" 
+            min="10" 
+            max="225" 
+            value={props.sliderValue} 
+            name="slider" 
+            onChange={props.handleUserInput} 
+          />
+
         </div>
       )
     
--- src/Containers/FractalMachine.jsx
+++ src/Containers/FractalMachine.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FractalCanvas from '../Components/FractalCanvas';
 import FractalMachineInput from '../Components/FractalMachineInput';
 
 const FractalMachine = () => {
 
+    const [userInputX, setUserInputX] = useState();
+    const [userInputY, setUserInputY] = useState();
+    const [sliderValue, setSliderValue] = useState(225);
+
+    const handleUserInput = (e) => {
+        console.log(e.target.value)
+        let num = parseInt(e.target.value)
+        if (e.target.name === "X") {
+            setUserInputX(num)
+        } else if (e.target.name === "Y") {
+            setUserInputY(num)
+        } else if (e.target.name === "slider") {
+            setSliderValue(num)
+        }
+    }
+
     return(
         <div>
             <FractalCanvas />
-            <FractalMachineInput />
+            <FractalMachineInput 
+                userInputX={userInputX}
+                userInputY={userInputY}
+                sliderValue={sliderValue}
+                handleUserInput={handleUserInput}
+            />
         </div>
     )
 } // end of FractalMachine fn
